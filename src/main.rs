7a19bd4af578f2a7914d08a991120e9bbb82d7d8
James H <hazeldinejames2@gmail.com>
2025-06-08 19:23:09 +1000

    Code gets a mixture of top and new stories concurrently

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use std::thread;
 use serde::Deserialize;
 use reqwest::blocking;
 
@@ -9,22 +10,35 @@ struct Story {
     url: String,
 }
 
+fn get_stories(cat: &str, id: &i32) -> Result<(), Box<dyn Error>> {
+    let id_string = id.to_string();
+    let url = format!("https://hacker-news.firebaseio.com/v0/item/{}.json", &id_string);
+    let resp = blocking::get(url)?;
+    let story: Story = resp.json()?;
+    println!("cat   : {}\ntitle : {}\nurl   : {}\n", cat, story.title, story.url);
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let ts_url = "https://hacker-news.firebaseio.com/v0/topstories.json";
     let resp = blocking::get(ts_url)?;
-    
-    let story_ids: Vec<i32> = resp.json()?;
-    let story_endpoints: Vec<String> = story_ids.iter().map(|x| {
-        let id_string = x.to_string();
-        format!("https://hacker-news.firebaseio.com/v0/item/{}.json", &id_string)
-    }).collect();
+    let ts_ids: Vec<i32> = resp.json()?;
 
-    for url in &story_endpoints[..30] {
-        let resp = blocking::get(url)?;
-        let story: Story = resp.json()?;
-        println!("title : {}", story.title);
-        println!("url   : {}\n", story.url);
+    let handle = thread::spawn(move || {
+        for id in &ts_ids[..15] {
+            let _ = get_stories("top", id);
+        }
+    });
+
+    let ns_url = "https://hacker-news.firebaseio.com/v0/newstories.json";
+    let resp = blocking::get(ns_url)?;
+    let ns_ids: Vec<i32> = resp.json()?;
+
+    for id in &ns_ids[..15] {
+        let _ = get_stories("new", id);
     }
+    
+    handle.join().unwrap();
 
     Ok(())
 }
