afd50dbbd0ba43d0f5cd91739c88c0430a404824
James H <hazeldinejames2@gmail.com>
2025-08-30 19:03:15 +1000

    Adding unit test for get_story

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use serde::Deserialize;
 use reqwest::Client;
 use futures::future::join_all;
+use std::cmp::PartialEq;
 
 #[derive(Deserialize, Debug)]
 struct Story {
@@ -9,19 +10,26 @@ struct Story {
     url: String,
 }
 
+impl PartialEq for Story {
+    fn eq(&self, other: &Self) -> bool {
+        self.title == other.title && self.url == other.url
+    }
+}
+
 pub async fn run(story_cat: String, num_stories: usize) -> Result<(), reqwest::Error> {
     
     let client = Client::new();
     let stories_url = format!("https://hacker-news.firebaseio.com/v0/{}stories.json", story_cat);
     let story_ids = client.get(stories_url).send().await?.json::<Vec<i32>>().await?;
-    
+   			 
     let mut handles = vec![];
     let mut i: usize = 0;
     while i < num_stories && i < story_ids.len() {
         
         if let Some(item) = story_ids.get(i) {
-            let handle = async {
-                match get_story(&client, item).await {
+           	let item_url = format!("https://hacker-news.firebaseio.com/v0/item/{}.json", item);
+			let handle = async {	     
+				match get_story(&client, item_url).await {
                     Ok(story) => {
                         println!("title : {}\nurl   : {}\n", story.title, story.url);
                     }
@@ -41,7 +49,47 @@ pub async fn run(story_cat: String, num_stories: usize) -> Result<(), reqwest::E
     Ok(())
 }
 
-async fn get_story(client: &Client, item: &i32) -> Result<Story, reqwest::Error> {
-    let item_url = format!("https://hacker-news.firebaseio.com/v0/item/{}.json", item);
+async fn get_story(client: &Client, item_url: String) -> Result<Story, reqwest::Error> {
     Ok(client.get(item_url).send().await?.json::<Story>().await?)
+}
+
+#[cfg(test)]
+mod tests {
+    use httpmock::prelude::*;
+    use serde_json::json;
+    use super::*;
+		
+	
+	#[tokio::test]
+	async fn get_story_test() {	
+		let client = Client::new();
+		let test_story = Story {
+			title: String::from("This is a test"),
+			url: String::from("https://www.test.com"),	
+        };
+		let server = MockServer::start();
+		let mock = server.mock(|when, then| {
+			when.method(GET)
+				.path("/v0/item/10000000.json");
+			then.status(200)
+				.json_body(json!({
+                    "by": "me",
+                    "descendants": 0,
+                    "id": 0,
+                    "kids": [10000001],
+                    "score": 0,
+                    "time": 0000000000,
+                    "title": "This is a test",
+                    "type": "story",
+                    "url": "https://www.test.com"
+                }));
+        });
+		let result = get_story(&client, server.url("/v0/item/10000000.json")).await;
+        
+		mock.assert();
+        //let result_unwrapped = result.unwrap();
+        assert!(result.is_ok());
+		assert_eq!(result.unwrap(), test_story);
+			
+	}
 }
